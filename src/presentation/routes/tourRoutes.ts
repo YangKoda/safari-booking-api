4bab0ad56eaf2ab94035c540eca9f826b3cc8819
YangKoda <thecompanymvmnt@gmail.com>
2026-01-20 02:53:36 +0100

    (routes): implemented tour routes

---
+++ src/presentation/routes/tourRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { TourController } from '../controllers/TourController';
+
+const router = Router();
+const tourController = new TourController();
+
+router.post('/', tourController.createTour);
+router.get('/', tourController.listTours);
+router.get('/:id', tourController.getTour);
+
+export { router as tourRoutes };
